b86989d3b327ad71e5eb148f6c8898d9c2225108
李会东 <hdli@credit2go.cn>
2018-04-12 17:53:56 +0800

    Swagger2

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -2,9 +2,12 @@ package com.example.demo.Controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Created by lihuidong on 2018-4-9.
@@ -15,14 +18,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value="添加用户", notes="根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @ResponseBody
-    @RequestMapping(value = "/add",method = RequestMethod.POST,
-            produces = {"application/json;charset=UTF-8"},consumes = {"application/x-www-form-urlencoded;charset=UTF-8"})
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
     public int addUser(User user){
         System.out.println(user.toString());
         return userService.addUser(user);
     }
 
+    @ApiIgnore
     @ResponseBody
     @RequestMapping(value = "/all/{pageNum}/{pageSize}", produces = {"application/json;charset=UTF-8"})
     public Object findAllUser(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize){
